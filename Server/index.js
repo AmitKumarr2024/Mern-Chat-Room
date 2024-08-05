5d76e43e290bd0b76e4e557fbe4f79278376c389
Amit kumar <codes.amitkumar@gmail.com>
2024-08-05 22:56:24 +0530

    favicon set

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,22 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
-
-
-const app = express();
 const port = process.env.PORT;
 
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   return res.status(200).send(`<H1>Hello Amit </H1>`);
 });
 
-app.listen(port,()=>{
-    console.log("Server is running at port:",port);
-    
-})
+app.listen(port, () => {
+  console.log("Server is running at port:", port);
+});
