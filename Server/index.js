09e74a8c4dd61da360797f4f3b6c51c13b57defb
Amit kumar <codes.amitkumar@gmail.com>
2024-08-15 02:27:01 +0530

    cors issue-2

--- Server/index.js
+++ Server/index.js
@@ -8,10 +8,11 @@ import UserRoute from "./src/context/Users/User_router.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./src/socket/index.js";
 
+const allowedOrigins = process.env.FRONTEND_URLS.split(','); // assuming FRONTEND_URLS is comma-separated
+
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = process.env.FRONTEND_URLS.split(','); // assuming FRONTEND_URLS is comma-separated
 
 app.use(
   cors({
