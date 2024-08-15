d8bc64b2418822239cff74905a0de259b256cb03
Amit kumar <codes.amitkumar@gmail.com>
2024-08-15 20:58:47 +0530

    new change-15

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
@@ -8,11 +7,11 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <RouterProvider router={router} future={{ v7_startTransition: true }}>
         <App />
       </RouterProvider>
     </Provider>
-  </React.StrictMode>
+
 );
