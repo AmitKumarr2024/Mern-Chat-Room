4af295c4af0cc81cc5e955aeadc95ba37626f9e2
Amit kumar <codes.amitkumar@gmail.com>
2024-08-16 12:37:05 +0530

    socket-10

--- Server/src/socket/index.js
+++ Server/src/socket/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URLS, // Allows multiple frontend URLs
+    origin: process.env.FRONTEND_URLS.split(','), // Allows multiple frontend URLs
     methods: ["GET", "POST"],
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   },
