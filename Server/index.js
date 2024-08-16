84d11eb76166e7c8eaad7bdb10f137384d04edba
Amit kumar <codes.amitkumar@gmail.com>
2024-08-16 17:25:57 +0530

    socket-29

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-dotenv.config(); // Make sure to load the environment variables before accessing them
+dotenv.config();  // Make sure to load the environment variables before accessing them
 import mongodb from "./src/config/mongodb.js";
 import UserRoute from "./src/context/Users/User_router.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./src/socket/index.js";
 
+
+
 app.use(express.json());
 app.use(cookieParser());
+
+
 
 // CORS middleware
 // const allowedOrigins = process.env.FRONTEND_URLS ? process.env.FRONTEND_URLS.split(',') : [];
@@ -23,20 +27,20 @@ app.use(cookieParser());
 //   methods: ["GET", "POST"],
 //   credentials: true,
 // }));
-
 app.use(
   cors({
     origin: process.env.FRONTEND_URLS ? "http://localhost:5173/" : [],
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
 app.use((req, res, next) => {
-  console.log("Method:", req.method);
-  console.log("Origin:", req.headers.origin); // Logs the origin
-  console.log("Path:", req.path);
-  console.log("Headers:", req.headers);
+  console.log('Method:', req.method);
+  console.log('Origin:', req.headers.origin); // Logs the origin
+  console.log('Path:', req.path);
+  console.log('Headers:', req.headers);
   next();
 });
 
@@ -45,12 +49,18 @@ app.use("/api/users", UserRoute);
 // Connect to MongoDB and start the server
 mongodb()
   .then(() => {
-    const port = process.env.PORT || 5000; // Default to port 5000 if PORT is not defined
+    const port = process.env.PORT || 5000;  // Default to port 5000 if PORT is not defined
     server.listen(port, () => {
       console.log(`Server is running at port: ${port}`);
     });
   })
   .catch((error) => {
     console.error("Error starting server:", error);
-    process.exit(1); // Exit the process if there’s an error
+    process.exit(1);  // Exit the process if there’s an error
   });
+
+
+
+
+
+  
